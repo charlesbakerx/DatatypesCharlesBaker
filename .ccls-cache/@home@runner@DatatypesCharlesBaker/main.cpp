a094172741ae446a371c17f9c8345829b4ac38f5
charlesbakerx <25694085-charlesbakerx@users.noreply.replit.com>
2023-09-01 17:52:40 +0000

    Added multi-line comment

--- .ccls-cache/@home@runner@DatatypesCharlesBaker/main.cpp
+++ .ccls-cache/@home@runner@DatatypesCharlesBaker/main.cpp
@@ -1,5 +1,12 @@
+/*
+Charles Baker, Programming Fundamentals I: COSC-1436-57001
+IDE: Replit
+Summary: Take an integer as an input
+*/
+
 #include <iostream>
+using namespace std;
 
 int main() {
-  std::cout << "Hello World!\n";
+  cout << "Hello World!\n";
 }
--- main.cpp
+++ main.cpp
@@ -1,5 +1,11 @@
+/*
+Charles Baker, Programming Fundamentals I: COSC-1436-57001
+IDE: Replit
+Summary: Take an integer representing pennies as input, calculate the conversion to dollars, quarters, nickels and dimes. Output the conversion amounts as well as remaining pennies.
+*/
+
 #include <iostream>
+using namespace std;
 
 int main() {
-  std::cout << "Hello World!\n";
 }
