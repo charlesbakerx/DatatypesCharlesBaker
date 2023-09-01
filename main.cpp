74ab6eec4a9cc85ed0f004e2c106afd48c0f73e6
charlesbakerx <25694085-charlesbakerx@users.noreply.replit.com>
2023-09-01 17:54:55 +0000

    Added variable declaration and input sections

--- main.cpp
+++ main.cpp
@@ -1,11 +1,23 @@
 /*
 Charles Baker, Programming Fundamentals I: COSC-1436-57001
 IDE: Replit
-Summary: Take an integer representing pennies as input, calculate the conversion to dollars, quarters, nickels and dimes. Output the conversion amounts as well as remaining pennies.
+Summary: Take an integer representing pennies as input, calculate the conversion to dollars, quarters, dimes, and nickels. Output the conversion amounts as well as remaining pennies.
 */
 
 #include <iostream>
 using namespace std;
 
 int main() {
+  // Variable Declaration
+  int dollars;
+  int quarters;
+  int dimes;
+  int nickels;
+  int pennies;
+
+  // Input
+  cout << "Please enter the number of pennies you have: ";
+  cin >> pennies;
+
+  
 }
