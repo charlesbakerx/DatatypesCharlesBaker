cf2f46b703e17fe1f9b9f934514f685d5463329c
charlesbakerx <25694085-charlesbakerx@users.noreply.replit.com>
2023-09-01 18:03:42 +0000

    Added process and output sections

--- main.cpp
+++ main.cpp
@@ -19,5 +19,28 @@ int main() {
   cout << "Please enter the number of pennies you have: ";
   cin >> pennies;
 
-  
+  // Process
+  // Convert dollars
+  dollars = pennies / 100;
+  // mod to determine leftover pennies
+  pennies = pennies % 100;
+  // Convert quarters
+  quarters = pennies / 25;
+  // mod to determine leftover pennies
+  pennies = pennies % 25;
+  // Convert dimes
+  dimes = pennies / 10;
+  // mod
+  pennies = pennies % 10;
+  // Convert nickels
+  nickels = pennies / 5;
+  // mod
+  pennies = pennies % 5;
+
+  // Output
+  cout << "Dollars = " << dollars << endl; 
+  cout << "Quarters = " << quarters << endl;
+  cout << "Dimes = " << dimes << endl;
+  cout << "Nickels = " << nickels<< endl;
+  cout << "Pennies = " << pennies << endl;
 }
